7b893fd428b28367948b1e3dd79d14c266184a86
Damian Simon Peter <theterminalguy@pop-os.localdomain>
2020-03-10 19:04:06 -0400

    Rollback remove snyk.io and add better bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -8,7 +8,7 @@ import profilePic from './profile-pic.jpeg'
 import { rhythm } from '../utils/typography'
 
 import InstagramIcon from 'react-icons/lib/fa/instagram'
-import FacebookIcon from 'react-icons/lib/fa/facebook-official'
+import LinkedInIcon from 'react-icons/lib/fa/linkedin-square'
 import TwitterIcon from 'react-icons/lib/fa/twitter-square'
 import GithubIcon from 'react-icons/lib/fa/github-square'
 
@@ -24,7 +24,7 @@ class Bio extends React.Component {
 
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`Damian Simon Peter`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
@@ -34,10 +34,14 @@ class Bio extends React.Component {
           }}
         /> 
         <p>
-          Written by <strong>Damian Simon Peter</strong> who lives in Lagos but works in <a href="https://www.hingeto.com/">Oakland, CA</a>.<br />
+          <strong>Damian Simon Peter</strong> is a programmer based in Kitchener, Ontario.
+          He currently works for <a href="https://smile.io" target="_blank" rel="noopener">Smile.io</a> as a Backend Developer focused on 
+          <a href="https://www.ruby-lang.org/en/" target="_blank" rel="noopener"> Ruby</a> and 
+          <a href="https://elixir-lang.org/" target="_blank" rel="noopener"> Elixir</a>
+        <br />
         <span style={{fontSize: "2em"}}>
+          <a href="https://www.linkedin.com/in/damian-simon-peter-2775a9100/"><LinkedInIcon /></a>
           <a href="https://www.instagram.com/theterminalguy/"><InstagramIcon/></a>
-          <a href="https://www.facebook.com/theterminalguy/"><FacebookIcon /></a>
           <a href="https://twitter.com/theterminalguy/"><TwitterIcon /></a>
           <a href="https://github.com/theterminalguy/"><GithubIcon /></a>
         </span>

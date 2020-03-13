f98e99ebb809ddd63dc06376903b2155fa079534
Damian Simon Peter <theterminalguy@pop-os.localdomain>
2020-03-12 21:57:56 -0400

    Add Google Analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-160628322-1',
       },
     },
     `gatsby-plugin-feed`,
